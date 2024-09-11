f18e70df1ec90fe091df463f350a21a3bc329374
shreni-kt <shreni@kindtech.co.in>
2024-09-11 23:48:59 +0530

    added custom box optimizations

--- src/components/CustomNode/index.js
+++ src/components/CustomNode/index.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import { Handle, Position } from '@xyflow/react';
-import logo from "../NodeTypes/Message/message-icon.png"
-import whatsapp from "../CustomNode/whatsapp.png"
-import "./index.css"
+import logo from "../NodeTypes/Message/message-icon.png";
+import whatsapp from "../CustomNode/whatsapp.png";
+import "./index.css";
 
-function CustomNode({ data = { label: "custom node" }, isConnectable = "true", }) {
-    console.log(data)
+function CustomNode({ data = { label: "text message" }, isConnectable = true }) {
     return (
         <>
             <Handle
                 type="target"
                 position={Position.Left}
-                style={{ background: '#555' }}
-                onConnect={(params) => console.log('handle onConnect2', params)}
+                style={{ background: 'rgb(186 183 183)', height: '5px', width: '5px' }}
+                onConnect={(params) => console.log('handle onConnect', params)}
                 isConnectable={isConnectable}
             />
             <div className="custom-box">
                 <div className="upper-half">
                     <div className="upper-half-container">
-                        <img src={logo} className="left-icon" alt='logo'/>
+                        <img src={logo} className="left-icon" alt="logo" />
                         <p className="heading">Send Message</p>
                     </div>
-                    <img src={whatsapp} className="right-icon" alt='logo'/>
+                    <img src={whatsapp} className="right-icon" alt="whatsapp" />
                 </div>
 
                 <div className="lower-half">
@@ -32,11 +31,11 @@ function CustomNode({ data = { label: "custom node" }, isConnectable = "true", }
                 type="source"
                 position={Position.Right}
                 id="a"
-                style={{ top: 10, background: '#555' }}
+                style={{ top: 10, background: 'rgb(186 183 183)', height: '5px', width: '5px' }}
                 isConnectable={isConnectable}
             />
         </>
     );
-};
+}
 
-export default CustomNode;
+export default React.memo(CustomNode);
