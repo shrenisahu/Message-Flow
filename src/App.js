64f9442898fcfbbda683bffc9e770f85adf04c1b
shreni-kt <shreni@kindtech.co.in>
2024-09-11 18:00:54 +0530

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+// import DropDragFlow from './components/DropAndDrag';
+import DropDragFlow from './components/DropDrag';
+import '@xyflow/react/dist/style.css';
+import "./App.css"
+import { Suspense, useState } from 'react';
+import {
+  ReactFlow,
+  ReactFlowProvider,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  Controls,
+  useReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+} from '@xyflow/react';
+
 
 function App() {
+  const initialNodes = [
+    {
+      id: '1',
+      type: 'selector',
+      data: { label: 'input node' },
+      position: { x: 250, y: 5 },
+    },
+  ];
+
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [showError, setShowError] = useState(false)
+  // const [onSave, setOnSave]= useState(true)
+  const handleSaveClick = () => {
+    let totalNode = nodes?.length;
+    const sett = new Set();
+    for (let i = 0; i < edges.length; i++)
+      sett.add(edges[i].target)
+    console.log("uniqueCount", edges, totalNode, sett.size)
+    if (totalNode - sett.size > 1 && totalNode > 1) {
+      setShowError(true)
+      console.log("error")
+    }
+    else
+      {
+        setShowError(false)
+        console.log("success")
+      }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main-app' >
+        <div className='main-app_header'>
+          <button
+            class="header-button"
+            onClick={() => handleSaveClick()}
+          >Save Changes</button>
+
+          {
+            showError && <button className="error-button" >Cannot Save Flow</button>
+          }
+        </div>
+
+
+        <DropDragFlow
+          nodes={nodes}
+          setNodes={setNodes}
+          onNodesChange={onNodesChange}
+          edges={edges}
+          setEdges={setEdges}
+          onEdgesChange={onEdgesChange}
+
+
+          handleSaveClick={handleSaveClick} />
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
