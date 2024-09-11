e6bf1a5182eddfcbc9887243dd7de0033f2cff65
shreni-kt <shreni@kindtech.co.in>
2024-09-11 23:56:18 +0530

    added settings  panel optimizations

--- src/components/SettingsPanel/index.js
+++ src/components/SettingsPanel/index.js
@@ -1,34 +1,30 @@
-import React from 'react'
-import logo from "./arrow.png"
-import "./index.css"
+import React from 'react';
+import logo from './arrow.png';
+import './index.css';
 
-const SettingsPanel = ({ editTextValue, handleEditChange, handleNodesPanel }) => {
-
-    return (
-
-        <div className='settings-panel_container'>
-            <div className='settings-panel_header'>
-                <img className='settings-panel_img' src={logo} onClick={() => handleNodesPanel(false)} alt='logo'/>
-                <span className='settings-panel_span'>Message</span>
-            </div>
-
-            <div className='settings-panel_input-container' >
-                <label className='settings-panel_input-container-label'> Text</label>
-                <textarea
-                    rows="5"
-                    placeholder="Enter text here..."
-                    value={editTextValue}
-                    onChange={handleEditChange}
-                    className='settings-panel_input'
-
-                    type='text '
-
-                >
-
-                </textarea>
-            </div>
+const SettingsPanel = ({ editTextValue, handleEditChange, handleNodesPanel }) => (
+    <div className='settings-panel_container'>
+        <div className='settings-panel_header'>
+            <img 
+                className='settings-panel_img' 
+                src={logo} 
+                onClick={() => handleNodesPanel(false)} 
+                alt='logo' 
+            />
+            <span className='settings-panel_span'>Message</span>
         </div>
-    )
-}
+
+        <div className='settings-panel_input-container'>
+            <label className='settings-panel_input-container-label'>Text</label>
+            <textarea
+                rows="5"
+                placeholder="Enter text here..."
+                value={editTextValue}
+                onChange={handleEditChange}
+                className='settings-panel_input'
+            />
+        </div>
+    </div>
+);
 
 export default SettingsPanel;
